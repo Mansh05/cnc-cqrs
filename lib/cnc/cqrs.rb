0d21ad22bf04b7b0195cbbfd3847056a63381d56
Manish Sharma <arnoltherasing@gmail.com>
2020-11-12 16:48:40 +0600

    first commit

---
+++ lib/cnc/cqrs.rb
@@ -0,0 +1,10 @@
+require "cnc/cqrs/version"
+
+module Cnc
+  module Cqrs
+    class Error < StandardError
+
+    end
+    # Your code goes here...
+  end
+end
---
+++ lib/cnc/cqrs/version.rb
@@ -0,0 +1,5 @@
+module Cnc
+  module Cqrs
+    VERSION = "0.1.0"
+  end
+end
